65af9619e80139bf2d14c2157b564ec3a55f0f98
Grayson Bartlet <grayson.bartlet@shaw.ca>
2017-06-19 15:52:37 -0600

    Basic post submission and listing

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -8,59 +8,66 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use AppBundle\Entity\Book;
+use AppBundle\Entity\Post;
 
 class DefaultController extends Controller
 {
     /**
+     * The home page. Lists posts and provides a link to create a new post.
      * @Route("/", name="homepage")
      */
-    public function indexAction(Request $request)
+    public function indexAction(Request $request, EntityManagerInterface $em)
     {
-        // replace this example code with whatever you need
+        return $this->listPosts($em);
         return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
         ]);
     }
 
-    /**
-     * @Route("/list", name="list")
-     */
-    public function listAction(Request $request, EntityManagerInterface $em) {
-        $repository = $em->getRepository('AppBundle:Book');
-        $books = $repository->findAll();
-        return new Response(print_r($books,true));
+    /*
+    * Display a list of posts
+    * @return $posts array TODO 
+    */
+    public function listPosts(EntityManagerInterface $em) {
+        $repository = $em->getRepository('AppBundle:Post');
+        $posts = $repository->findAll(); //TODO: sort by date
+        return new Response(print_r($posts,true));
     }
 
+
     /**
+     * Page for user to submit a post.
+     * TODO: extract form to a class
      * @Route("/create")
      */
     public function createAction(Request $request, EntityManagerInterface $em){
-        $book = new Book();
+        $post = new Post();
 
-        //create form to fill book with data
-        $form = $this->createFormBuilder($book)
-            ->add('name', TextType::class)
-            ->add('author', TextType::class)
-            ->add('date', DateType::class)
-            ->add('pdf', TextType::class)
-            ->add('save', SubmitType::class, array('label' => 'Create Book'))
+        //create form to fill post with data
+        $form = $this->createFormBuilder($post)
+            ->add('title', TextType::class)
+            ->add('email', TextType::class)
+            ->add('description', TextareaType::class) 
+            ->add('date', DateType::class)//TODO: fill automatically
+            ->add('save', SubmitType::class, array('label' => 'Create Post'))
             ->getForm();
 
         $form->handleRequest($request);
 
+        //TODO: validate emails and description length
         if ($form->isSubmitted() ) {
             if($form->isValid()){
-                $book = $form->getData();
+                $post = $form->getData();
 
-                // save book to db
-                $em->persist($book);
+                // save post to db
+                $em->persist($post);
                 $em->flush();
 
                 //return $this->redirectToRoute('task_success');
-                return new Response("Submitted book!");
+                return new Response("Submitted post!");
             }
             else{
                 return new Response("Not valid!");
